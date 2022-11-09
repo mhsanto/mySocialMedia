4bead1632988c6d8f8d73cb3193512ef35d51dd7
Mahmodul Hasan <nicesanto92@gmail.com>
2022-11-09 16:03:48 +0600

    fixed some issues

--- script.js
+++ script.js
@@ -1,154 +1,141 @@
 // sidebar
 let menuItem = document.querySelectorAll(".menu-item");
-let notificationPopup = document.querySelector('.notification-popup');
-
-
-
-// ===========messages=========
-let messageNotification = document.querySelector('#messages-notification');
+let notificationPopup = document.querySelector(".notification-popup");
+// ===========messages========
+let messageNotification = document.querySelector("#messages-notification");
 let messages = document.querySelector(".messages");
 
-let message = document.querySelectorAll('.message');
-let messageSearch = document.querySelector('#message-search');
+let message = document.querySelectorAll(".message");
+let messageSearch = document.querySelector("#message-search");
 // theme
-let theme = document.querySelector('#theme');
-let themeModal = document.querySelector('.customize-theme');
+let theme = document.querySelector("#theme");
+let themeModal = document.querySelector(".customize-theme");
 // font size
-let fontSizes = document.querySelectorAll('.choose-size span');
+let fontSizes = document.querySelectorAll(".choose-size span");
 
-var root = document.querySelector(':root');
+var root = document.querySelector(":root");
 
-const Bg1 = document.querySelector('.bg-1');
-const Bg2 = document.querySelector('.bg-2');
-const Bg3 = document.querySelector('.bg-3');
-
+const Bg1 = document.querySelector(".bg-1");
+const Bg2 = document.querySelector(".bg-2");
+const Bg3 = document.querySelector(".bg-3");
 
 let changeActiveItem = () => {
-    menuItem.forEach(item => {
-        item.classList.remove('active');
-     
-    })
-}
+  menuItem.forEach((item) => {
+    item.classList.remove("active");
+  });
+};
 // Notification will show when clicked
-menuItem.forEach(item => {
-    item.addEventListener('click', () => {
-        changeActiveItem();
-        item.classList.add('active');
-        if (item.id != 'notifications') {
-            document.querySelector('.notification-popup').style.display = 'none';
-        } else {
-            document.querySelector('.notification-popup').style.display = 'block';
-            document.querySelector('.notification-count').style.display = 'none';
-
-        }
-    })
+menuItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    changeActiveItem();
+    item.classList.add("active");
+    if (item.id != "notifications") {
+      document.querySelector(".notification-popup").style.display = "none";
+    } else {
+      document.querySelector(".notification-popup").style.display = "block";
+      document.querySelector(".notification-count").style.display = "none";
+    }
+  });
 });
 // messages will be highlighted if its clicked
-messageNotification.addEventListener('click', () => {
-    messages.style.boxShadow = "0 0 1rem var(--color-primary)";
-    messageNotification.querySelector('.notification-count').style.display = 'none';
+messageNotification.addEventListener("click", () => {
+  messages.style.boxShadow = "0 0 1rem var(--color-primary)";
+  messageNotification.querySelector(".notification-count").style.display =
+    "none";
 
-
-    setTimeout(() => {
-        messages.style.boxShadow = "none";
-
-    }, 2000);
-})
+  setTimeout(() => {
+    messages.style.boxShadow = "none";
+  }, 2000);
+});
 // THEME CUSTOMIZATION
 // open theme
 const openThemeModal = () => {
-    themeModal.style.display = 'grid';
-}
-theme.addEventListener('click', openThemeModal);
-//  close theme 
+  themeModal.style.display = "grid";
+};
+theme.addEventListener("click", openThemeModal);
+//  close theme
 
 const closeThemeModal = (e) => {
-    if (e.target.classList.contains('customize-theme')) {
-        themeModal.style.display = 'none'
-    }
-}
-themeModal.addEventListener('click', closeThemeModal);
+  if (e.target.classList.contains("customize-theme")) {
+    themeModal.style.display = "none";
+  }
+};
+themeModal.addEventListener("click", closeThemeModal);
 
 //  changing fontSizes
-const removeSizeSelector = ()=>{
-    fontSizes.forEach(size =>{
-        size.classList.remove('active');
-    })
-}
+const removeSizeSelector = () => {
+  fontSizes.forEach((size) => {
+    size.classList.remove("active");
+  });
+};
 
-fontSizes.forEach(size => {
-    
-   
-    size.addEventListener('click',() =>{
-        removeSizeSelector();
-        let fontSize;
-       size.classList.toggle('active');
-    if (size.classList.contains('font-size-1')) {
-        fontSize = '10px';
-        root.style.setProperty('--sticky-top-left', '5.4rem');
-        root.style.setProperty('--sticky-top-right', '5.4rem');
-    } else if (size.classList.contains('font-size-2')) {
-        fontSize = '13px';
-        root.style.setProperty('--sticky-top-left', '5.4rem');
-        root.style.setProperty('--sticky-top-right', '-7rem');
-    } else if (size.classList.contains('font-size-3')) {
-        fontSize = '16px';
-        root.style.setProperty('--sticky-top-left', '-2rem');
-        root.style.setProperty('--sticky-top-right', '-17rem');
-    } else if (size.classList.contains('font-size-4')) {
-        fontSize = '19px';
-        root.style.setProperty('--sticky-top-left', '-5rem');
-        root.style.setProperty('--sticky-top-right', '-25rem');
+fontSizes.forEach((size) => {
+  size.addEventListener("click", () => {
+    removeSizeSelector();
+    let fontSize;
+    size.classList.toggle("active");
+    if (size.classList.contains("font-size-1")) {
+      fontSize = "10px";
+      root.style.setProperty("--sticky-top-left", "5.4rem");
+      root.style.setProperty("--sticky-top-right", "5.4rem");
+    } else if (size.classList.contains("font-size-2")) {
+      fontSize = "13px";
+      root.style.setProperty("--sticky-top-left", "5.4rem");
+      root.style.setProperty("--sticky-top-right", "-7rem");
+    } else if (size.classList.contains("font-size-3")) {
+      fontSize = "16px";
+      root.style.setProperty("--sticky-top-left", "-2rem");
+      root.style.setProperty("--sticky-top-right", "-17rem");
+    } else if (size.classList.contains("font-size-4")) {
+      fontSize = "19px";
+      root.style.setProperty("--sticky-top-left", "-5rem");
+      root.style.setProperty("--sticky-top-right", "-25rem");
     }
-    document.querySelector('html').style.fontSize = fontSize;
-    
-   })
-})
-
+    document.querySelector("html").style.fontSize = fontSize;
+  });
+});
 
 // theme background values
 let lightColorLightness;
 let darkColorLightness;
 let whiteColorLightness;
 
-const changeBG =() =>{
-    root.style.setProperty('--light-color-lightness',lightColorLightness);
-    root.style.setProperty('--white-color-lightness',whiteColorLightness);
-    root.style.setProperty('--dark-color-lightness',darkColorLightness);
-}
+const changeBG = () => {
+  root.style.setProperty("--light-color-lightness", lightColorLightness);
+  root.style.setProperty("--white-color-lightness", whiteColorLightness);
+  root.style.setProperty("--dark-color-lightness", darkColorLightness);
+};
 
-Bg1.addEventListener('click',() =>{
-    // add active class
-    Bg1.classList.add('active');
-    Bg2.classList.remove('active');
-    Bg3.classList.remove('active');
-    
-    window.location.reload();
-});
-Bg2.addEventListener('click',() =>{
-    darkColorLightness = '95%';
-    whiteColorLightness ='0%';
-    lightColorLightness ='15%';
-    
-    // add active class
-    Bg2.classList.add('active');
-    Bg1.classList.remove('active');
-    Bg3.classList.remove('active');
-    changeBG();
-});
-Bg3.addEventListener('click',() =>{
-    darkColorLightness = '95%';
-    whiteColorLightness ='10%';
-    lightColorLightness ='0%';
-    
-    // add active class
-    Bg3.classList.add('active');
-    Bg2.classList.remove('active');
-    Bg1.classList.remove('active');
-    changeBG();
-});
+Bg1.addEventListener("click", () => {
+  // add active class
+  Bg1.classList.add("active");
+  Bg2.classList.remove("active");
+  Bg3.classList.remove("active");
 
+  window.location.reload();
+});
+Bg2.addEventListener("click", () => {
+  darkColorLightness = "95%";
+  whiteColorLightness = "0%";
+  lightColorLightness = "15%";
 
+  // add active class
+  Bg2.classList.add("active");
+  Bg1.classList.remove("active");
+  Bg3.classList.remove("active");
+  changeBG();
+});
+Bg3.addEventListener("click", () => {
+  darkColorLightness = "95%";
+  whiteColorLightness = "10%";
+  lightColorLightness = "0%";
+
+  // add active class
+  Bg3.classList.add("active");
+  Bg2.classList.remove("active");
+  Bg1.classList.remove("active");
+  changeBG();
+});
 
 // // search chat
 // const searchMessage = () => {>
